fc37739261952bf0f37a57ca7598b521b4372e54
HubertSkrobot <hubertskrobotx@gmail.com>
2026-01-07 13:31:03 +0100

    Ekrant końcowe w nowej klasie
    Added EndScreen member to Game class.

--- Game.h
+++ Game.h
@@ -6,31 +6,27 @@
 #include "Paddle.h"
 #include "klocek.h"
 #include "GameState.h" 
+#include "EndScreen.h"
 
 class Game {
 private:
     sf::Font font;
     sf::Text scoreLabel;
-    sf::Text gameOverText;
-    sf::RectangleShape gameOverRect;
-    sf::Text restartText;
-
-    sf::Text winText;
-    sf::RectangleShape winRect;
+    EndScreen endScreen;
 
 public:
     Ball ball;
     Paddle paddle;
     std::vector<Brick> bricks;
     int score;
-    bool gameOver; //ekran przegranej
-    bool gameWon; //wygranej
+    bool gameOver;
+    bool gameWon;
     sf::Vector2f brickSize;
     Game();
-    GameState handleEvent(sf::Event& event); 
+    GameState handleEvent(sf::Event& event);
     void update(sf::Time dt, sf::Vector2f windowSize);
     void render(sf::RenderTarget& target);
     void reset();
 
-    bool isGameOver() const { return gameOver; } //sprawdzenie czy pilka spadla
+    bool isGameOver() const { return gameOver; }
 };
